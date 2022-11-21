4569d8f0b4d20cadde602b4e64e7b7f565ce1ce0
MiRTMq <118728863+MiRTMq@users.noreply.github.com>
2022-11-21 14:32:34 +0200

    Add files via upload

---
+++ ConsoleApplication0.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+void Sum()
+{
+    int sum = 0; float x, y;
+
+    cout << "Enter x:\n";
+    cin >> x;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        sum += i;
+    }
+
+    y = tan(sum + 2 * x);
+
+    cout << "\tY = " << setprecision(4) << y;
+}
+
+void Mult()
+{
+    int x;  double z, mul = 1;
+
+    cout << "Enter x:\n";
+    cin >> x;
+
+    for (int k = 1; k <= 10; k++)
+    {
+        mul *= pow(k, 3);
+    }
+
+    z = pow(x, 5) + mul;
+
+    cout << "\tZ = " << setprecision(4) << z;
+}
+
+int main()
+{
+    
+ //////////////////////////////////////// ЗАВДАННЯ 1
+   
+    //Sum();
+
+ //////////////////////////////////////// ЗАВДАННЯ 2
+
+    Mult();
+    cout << "new!" << endl;
+
+    return 0;
+}
+
