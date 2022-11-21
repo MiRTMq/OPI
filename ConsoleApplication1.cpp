84db42889d47dfd287de24eb0f20339083092536
MiRTMq <kalpakchy7@gmail.com>
2022-11-21 16:45:03 +0200

    File edited

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-	for (int i = 0; i <= 5; i++)
+	for (int i = 0; i <= 8; i++)
 	{
 		for (int j = 0; j < i; j++)
 		{
-			cout << '*';
+			cout << '0';
 		}
 		cout << endl;
 	}
